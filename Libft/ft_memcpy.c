cc4d2ae088f9cf8b23fc84e016510dbd1d27e0ae
tpereira <tpereira@student.42lisboa.com>
2021-02-13 11:08:19 +0000

    update repo

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpereira <tpereira@42lisboa.com>           +#+  +:+       +#+        */
+/*   By: tpereira <tpereira@student.42Lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 21:18:08 by tpereira          #+#    #+#             */
-/*   Updated: 2021/02/10 22:04:24 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/02/11 14:33:30 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,18 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char 		*dest2;
-    const char	*src2;
-    
+	char		*dest2;
+	const char	*src2;
+
 	dest2 = (char *)dest;
 	src2 = (const char*)src;
-	if((dest2 != NULL) && (src2 != NULL))
-    {
-        while (n)
-        {
-            *(dest2++) = *(src2++);
-            --n;
-        }
-    }
-    return dest;
-}
-
-int 	main()
-{
-    char 	src[20] = "How Are you ?";
-    char 	dest[20] = {0};
-
-    ft_memcpy(dst,src,sizeof(src));
-	printf("dest = %s\n", dest);
-    return 0;
+	if ((dest2 != NULL) && (src2 != NULL))
+	{
+		while (n)
+		{
+			*(dest2++) = *(src2++);
+			--n;
+		}
+	}
+	return (dest);
 }
--- Libft/main.c
+++ Libft/main.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpereira <tpereira@student.42Lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/08 22:45:40 by tpereira          #+#    #+#             */
-/*   Updated: 2021/02/11 14:27:40 by tpereira         ###   ########.fr       */
+/*   Created: 2021/02/10 22:16:47 by tpereira          #+#    #+#             */
+/*   Updated: 2021/02/11 14:26:24 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+int	main(void)
 {
-	size_t	i;
+	char	src[20];
 
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)s)[i] = c;
-		i++;
-	}
-	return (s);
+	src[0] = 'H';
+	src[1] = 'e';
+	src[2] = 'l';
+	src[3] = 'l';
+	src[4] = 'o';
+	src[5] = ' ';
+	src[6] = 'u';
+	src[7] = '!';
+	src[0] = '\0';
+	ft_bzero(src, sizeof(src));
+	printf("src = %s\n", src);
+	return (0);
 }
