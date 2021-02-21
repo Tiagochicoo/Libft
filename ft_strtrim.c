62c005fe46df35b5b42cc70342364d9b8d2a8050
tpereira <tpereira@student.42lisboa.com>
2021-02-21 22:38:05 +0000

    ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/21 14:40:35 by tpereira          #+#    #+#             */
+/*   Updated: 2021/02/21 22:21:38 by tpereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	is_set(char const *set, char c)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*s2;
+	size_t	i;
+	size_t	from;
+	size_t	to;
+
+	from = 0;
+	to = ft_strlen(s1);
+	while (is_set(set, s1[from]) && s1[from])
+		from++;
+	while (is_set(set, s1[to - 1]) && from < to)
+		to--;
+	s2 = (char *)malloc(sizeof(*s1) * (to - from + 1));
+	if (s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (from < to)
+	{
+		s2[i] = s1[from];
+		from++;
+		i++;
+	}
+	s2[i++] = '\0';
+	return (s2);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpereira <tpereira@42lisboa.com>           +#+  +:+       +#+        */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 16:16:51 by tpereira          #+#    #+#             */
-/*   Updated: 2021/02/20 18:06:31 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/02/20 22:05:31 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ char	*ft_substr(char const *s, size_t start, size_t len)
 	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (sub == NULL)
 		return (NULL);
+	if (start == 0 && len == 0)
+		return (NULL);
 	while (i < len && (i + start < ft_strlen(s)))
 	{
 		sub[i] = s[start + i];
-	   i++;	
+		i++;
 	}
 	sub[i] = '\0';
 	return (sub);
