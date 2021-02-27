a214a0fc3051776616e3d8679e2d784bc3ddacd7
tpereira <tpereira@student.42lisboa.com>
2021-02-27 08:55:38 +0000

    old split

--- ft_split.c
+++ ft_split.c
@@ -6,75 +6,129 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 21:26:41 by tpereira          #+#    #+#             */
-/*   Updated: 2021/02/23 22:04:25 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/02/27 08:55:22 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_get_next_str(char **pos_in_str, char *charset, int *next_str_len)
+char    *ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int		i;
-	char	*str_start;
+        unsigned int            i;
 
-	*next_str_len = 0;
-	str_start = 0;
-	i = 0;
-	while ((*pos_in_str)[i])
-	{
-		if (ft_strchr((*pos_in_str)[i], charset) != 0 && str_start != 0)
-		{
-			*pos_in_str = str_start + *next_str_len;
-			return (str_start);
-		}
-		else if (!ft_strchr((*pos_in_str)[i], charset) && str_start == 0)
-			str_start = &(*pos_in_str)[i];
-		if (!ft_strchr((*pos_in_str)[i], charset))
-			*next_str_len = *next_str_len + 1;
-		i++;
-	}
-	*pos_in_str = str_start + *next_str_len;
-	if (*next_str_len == 0)
-		return (0);
-	return (str_start);
+        i = 0;
+        while (src[i] && i < n)
+        {
+                dest[i] = src[i];
+                i++;
+        }
+        while (i < n)
+        {
+                dest[i] = '\0';
+                i++;
+        }
+        dest[i] = '\0';
+        return (dest);
 }
 
-char	**ft_build_tab(char *str, char *charset)
+int             ft_is_separator(char c, char *charset)
 {
-	int		nb_str;
-	char	**strs;
-	int		next_str_len;
-	char	*pos_in_str;
+        int             i;
 
-	nb_str = 0;
-	next_str_len = 0;
-	pos_in_str = str;
-	while (ft_get_next_str(&pos_in_str, charset, &next_str_len))
-		nb_str++;
-	if (!(strs = (char **)malloc(sizeof(char *) * (nb_str + 1))))
-		return (0);
-	return (strs);
+        i = 0;
+        while (charset[i])
+        {
+                if (c == charset[i])
+                        return (1);
+                i++;
+        }
+        return (0);
 }
 
-char	**ft_split(char *str, const char *charset)
+char    *ft_get_next_str(char **pos_in_str, char *charset, int *next_str_len)
 {
-	char	**strs;
-	int		next_str_len;
-	char	*next_str;
-	char	*pos_in_str;
-	int		i;
+        int             i;
+        char    *str_start;
 
-	if (!(strs = ft_build_tab(str, charset)))
-		return (0);
-	i = 0;
-	pos_in_str = str;
-	while ((next_str = ft_get_next_str(&pos_in_str, charset, &next_str_len)))
-	{
-		if (!(strs[i] = (char *)malloc(sizeof(char) * next_str_len + 1)))
-			return (0);
-		ft_strlcpy(strs[i], next_str, next_str_len);
-		i++;
-	}
-	strs[i] = 0;
-	return (strs);
+        *next_str_len = 0;
+        str_start = 0;
+        i = 0;
+        while ((*pos_in_str)[i])
+        {
+                if (ft_is_separator((*pos_in_str)[i], charset) && str_start != 0)
+                {
+                        *pos_in_str = str_start + *next_str_len;
+                        return (str_start);
+                }
+                else if (!ft_is_separator((*pos_in_str)[i], charset) && str_start == 0)
+                        str_start = &(*pos_in_str)[i];
+                if (!ft_is_separator((*pos_in_str)[i], charset))
+                        *next_str_len = *next_str_len + 1;
+                i++;
+        }
+        *pos_in_str = str_start + *next_str_len;
+        if (*next_str_len == 0)
+                return (0);
+        return (str_start);
+}
+
+char    **ft_build_tab(char *str, char *charset)
+{
+        int             nb_str;
+        char    **strs;
+        int             next_str_len;
+        char    *pos_in_str;
+
+        nb_str = 0;
+        next_str_len = 0;
+        pos_in_str = str;
+        while (ft_get_next_str(&pos_in_str, charset, &next_str_len))
+                nb_str++;
+        if (!(strs = (char **)malloc(sizeof(char *) * (nb_str + 1))))
+                return (0);
+        return (strs);
+}
+
+char    **ft_split(char *str, char *charset)
+{
+        char    **strs;
+        int             next_str_len;
+        char    *next_str;
+        char    *pos_in_str;
+        int             i;
+
+        if (!(strs = ft_build_tab(str, charset)))
+                return (0);
+        i = 0;
+        pos_in_str = str;
+        while ((next_str = ft_get_next_str(&pos_in_str, charset, &next_str_len)))
+        {
+                if (!(strs[i] = (char *)malloc(sizeof(char) * next_str_len + 1)))
+                        return (0);
+                ft_strncpy(strs[i], next_str, next_str_len);
+                i++;
+        }
+        strs[i] = 0;
+        return (strs);
+}
+
+int             main(int argc, char **argv)
+{
+        int             index;
+        char    **split;
+
+        argc = argc + 0;
+        split = ft_split("My name is tiago-chico", " -");
+        //split = ft_split(argv[1], argv[2]);
+        (void)argv;
+        index = 0;
+        printf("tab start\n");
+        while (split[index])
+        {
+                printf("tab[%d]: $%s$\n", index, split[index]);
+                index++;
+        }
+        printf("tab end\n");
+        return (0);
 }
